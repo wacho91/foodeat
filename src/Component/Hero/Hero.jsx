c13fbd21d8a3947edf4bcebdf5fe207649d76542
wacho91 <criskol.71@gmail.com>
2024-11-27 11:12:17 -0500

    Hero Component Complete

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -4,10 +4,10 @@ import HeroImg from "../../assets/1.png";
 const Hero = () => {
   return (
     <div>
-        <div>
+        <div className="container grid grid-cols-1 md:grid-cols-2 gap-8 min-h-[600px]">
             {/* text section */}
-            <div>
-                <h1>
+            <div className="flex flex-col justify-center gap-8 text-center md:text-left pt-24 md:p-0 pb-10">
+                <h1 className="text-4xl lg:text-6xl font-semibold">
                     Delicious Food Is Waiting For You
                 </h1>
                 <p>
@@ -15,17 +15,17 @@ const Hero = () => {
                     assumenda qui ipsam id officiis modi cumque minus dolores placeat
                     explicabo.
                 </p>
-                <div>
-                    <button>
+                <div className="flex gap-4 items-center md:justify-start justify-center">
+                    <button className="primary-btn hover:scale-105 duration-200">
                         Food Menu
                     </button>
-                    <button>
+                    <button className="secondary-btn text-black hover:scale-105 duration-200">
                         Book Table
                     </button>
                 </div>
             </div>
             {/* image section */}
-            <div>
+            <div className="flex flex-col justify-center">
                 <img 
                     src={HeroImg} 
                     alt="" 
