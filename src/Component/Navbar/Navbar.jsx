d5e2fffa1d848f67e1bbf58a31e595b0c23b87c8
wacho91 <criskol.71@gmail.com>
2024-11-27 10:53:56 -0500

    Navbar Component Complete

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import Profile from "../../assets/profile.png"
 
 const Navbar = () => {
   return (
-    <div>
-        <div>
+    <div className=" py-4 lg:py-6">
+        <div className="container flex justify-between items-center">
             {/* logo section */}
             <div>
                 <p className="text-3xl lg:text-4xl font-semibold">
@@ -13,15 +13,15 @@ const Navbar = () => {
                 </p>
             </div>
             {/* Menu section */}
-            <div>
-                <ul>
-                    <li>Home</li>
-                    <li>Menu</li>
-                    <li>About</li>
+            <div className="flex justify-center items-center gap-10">
+                <ul className="gap-8 hidden sm:flex">
+                    <li className="hover:border-b-2 border-primary uppercase">Home</li>
+                    <li className="hover:border-b-2 border-primary uppercase">Menu</li>
+                    <li className="hover:border-b-2 border-primary uppercase">About</li>
                 </ul>
                 {/* login section */} 
-                <div>
-                    <img src={Profile} alt="" />
+                <div className="flex gap-4 items-center">
+                    <img src={Profile} alt="" className="w-10 rounded-full "/>
                     <IoIosArrowDown />
                 </div>
             </div>
