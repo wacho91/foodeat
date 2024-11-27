6e7185a9d788451aa761103285a8b7d6ab852a6d
wacho91 <criskol.71@gmail.com>
2024-11-27 10:47:53 -0500

    Arquitecture Navbar Complete

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
 
+import { IoIosArrowDown } from "react-icons/io"
+import Profile from "../../assets/profile.png"
 
 const Navbar = () => {
   return (
     <div>
-        Navbar Component 
+        <div>
+            {/* logo section */}
+            <div>
+                <p className="text-3xl lg:text-4xl font-semibold">
+                    FOOD <span className="text-primary">EAT</span>
+                </p>
+            </div>
+            {/* Menu section */}
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Menu</li>
+                    <li>About</li>
+                </ul>
+                {/* login section */} 
+                <div>
+                    <img src={Profile} alt="" />
+                    <IoIosArrowDown />
+                </div>
+            </div>
+        </div> 
     </div>
   )
 }
