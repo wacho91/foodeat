fb71eca6fe8f5305622864b01ddd420b66c5f7d8
wacho91 <criskol.71@gmail.com>
2024-11-27 11:37:49 -0500

    TopList Component Complete

--- src/Component/TopList/TopList.jsx
+++ src/Component/TopList/TopList.jsx
@@ -29,25 +29,29 @@ const FoodData = [
 
 const TopList = () => {
   return (
-    <div>
+    <div  className="container py-14">
         {/* header section */}
-        <div>
-            <h1>Top List</h1>
+        <div className="text-center mb-12">
+            <h1 className="text-4xl font-semibold">Top List</h1>
             <p>Our top list</p>
         </div>
         {/* card section */}
-        <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 gap-8">
             {FoodData.map((item, index) => (
-                <div key={index}>
+                <div 
+                    key={index}
+                    className="bg-white/50 p-5 lg:p-6 rounded-3xl hover:scale-110 transition duration-300"
+                >
                     <img 
                         src={item.image} 
                         alt="" 
+                        className="w-60 sm:w-40  lg:w-[240px] mx-auto object-cover rounded-full img-shadow"
                     />
-                    <div>
-                        <p>{item.rating}</p>
-                        <p>{item.name}</p>
+                    <div className="space-y-2">
+                        <p className="text-red-500">{item.rating}</p>
+                        <p className="text-lg font-semibold">{item.name}</p>
                         <p>{item.desc}</p>
-                        <p>{item.price}</p>
+                        <p className="text-lg font-semibold">{item.price}</p>
                     </div>
                 </div>
             ))}
