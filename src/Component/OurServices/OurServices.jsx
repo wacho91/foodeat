c1d50d6cba19cc49139a96756775c0ee841eff37
wacho91 <criskol.71@gmail.com>
2024-11-28 11:01:51 -0500

    OurServices Component Complete

--- src/Component/OurServices/OurServices.jsx
+++ src/Component/OurServices/OurServices.jsx
@@ -5,28 +5,28 @@ import { MdFoodBank, MdOutlineFastfood } from "react-icons/md"
 
 const OurServices = () => {
   return (
-    <div>
+    <div className="container py-12">
         {/* header section */}
-        <div>
-            <h1>Our Services</h1>
+        <div className="text-center">
+            <h1 className="text-4xl font-semibold">Our Services</h1>
         </div>
         {/* icons section */}
-        <div>
-            <div>
-                <FaMobileScreen />
-                <p>Online Booking</p>
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-12 mt-12">
+            <div className="flex justify-center items-center gap-3">
+                <FaMobileScreen className="text-2xl" />
+                <p className="text-xl font-semibold">Online Booking</p>
             </div>
-            <div>
-                <MdOutlineFastfood />
-                <p>Fast Food</p>
+            <div className="flex justify-center items-center gap-3">
+                <MdOutlineFastfood className="text-2xl" />
+                <p className="text-xl font-semibold">Fast Food</p>
             </div>
-            <div>
-                <MdFoodBank />
-                <p>Healthy Food</p>
+            <div className="flex justify-center items-center gap-3">
+                <MdFoodBank className="text-2xl" />
+                <p className="text-xl font-semibold">Healthy Food</p>
             </div>
-            <div>
-                <CiDeliveryTruck />
-                <p>Delivery</p>
+            <div className="flex justify-center items-center gap-3">
+                <CiDeliveryTruck className="text-2xl" />
+                <p className="text-xl font-semibold">Delivery</p>
             </div>
         </div>
     </div>
