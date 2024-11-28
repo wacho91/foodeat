0d29f653454ec68b855e08e0e5b13d70a54ea907
wacho91 <criskol.71@gmail.com>
2024-11-28 10:38:07 -0500

    Arquitecture Component Banner

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,9 +1,30 @@
 
+import BannerImage from "../../assets/2.png";
+
 const Banner = () => {
   return (
-    <div>
-        Banner Component 
-    </div>
+    <>
+        <div>
+            <div>
+                {/* img section */}
+                <div>
+                    <img 
+                        src={BannerImage} 
+                        alt="" 
+                    />
+                </div>
+                {/* text section */}
+                <div>
+                    <h1>Food Is Always Good</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Temporibus laboriosam vitae accusamus assumenda, nobis perferendis
+                        ducimus! Itaque similique, recusandae nulla{" "}
+                    </p>
+                </div>
+            </div>
+        </div> 
+    </>
   )
 }
 
